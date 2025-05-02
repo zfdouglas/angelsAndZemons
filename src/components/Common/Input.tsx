71f74a40d6f62b5662ebc8b6d95bcfe15baeca39
zfdouglas <zacharyfdouglas@gmail.com>
2025-05-01 20:10:31 -0400

    feat: change input color

--- src/components/Common/Input.tsx
+++ src/components/Common/Input.tsx
@@ -5,7 +5,7 @@ interface InputProps {
 export const Input = ({text, setText}: InputProps) => {
     return(
         <input 
-            style={{width: '80%', backgroundColor: 'transparent', borderRadius: '10px'}} 
+            style={{width: '80%', backgroundColor: 'transparent', borderRadius: '10px', fontWeight: 600, color: 'maroon'}} 
             type={'text'}
             value={text}
             onChange={(e)=> setText(e.currentTarget.value)}
